4e33ccc381fb6584fbd9710e4c21c102650ef359
ASBrouwers <annebrouwers1998@gmail.com>
2018-05-09 23:03:11 +0200

    First prototype

---
+++ TransportInfo/src/UnfoldingWindow.java
@@ -0,0 +1,48 @@
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.MarkerManager;
+import de.fhpotsdam.unfolding.providers.OpenStreetMap;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import processing.core.PApplet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+/**
+ * Created by s141753 on 8-5-2018.
+ */
+public class UnfoldingWindow extends PApplet {
+
+    static UnfoldingMap map;
+    static MarkerManager markerManager;
+
+    public void setup() {
+        this.frameRate(10);
+        size(1000,900);
+        map = new UnfoldingMap(this, new OpenStreetMap.OpenStreetMapProvider());
+
+        map.setTweening(true);
+        Location center = new Location(49f, 5f);
+        map.zoomAndPanTo(6, center);
+        MapUtils.createDefaultEventDispatcher(this, map);
+        markerManager = map.getDefaultMarkerManager();
+    }
+
+    public void draw() {
+        map.draw();
+    }
+
+    public void addMarkers(List<Marker> markers) {
+        markerManager.addMarkers(markers);
+    }
+
+    public void clearMarkers() {
+        markerManager.clearMarkers();
+    }
+
+
+}
